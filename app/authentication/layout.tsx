3ce3f20d57b5606af65cece87270183217a55c72
comillas20 <jinojoycomillas20@gmail.com>
2023-12-30 21:23:56 +0800

    test on vercel

--- app/authentication/layout.tsx
+++ app/authentication/layout.tsx
@@ -13,7 +13,9 @@ export default async function SignInPage({
 	const session = await getServerSession(options);
 	return session && session.user.id ? (
 		<div className="flex h-screen flex-col items-center justify-center gap-4">
-			<span>You are already signed in. No need to repeat things.</span>
+			<span>You are already signed in. No need to sign-in again.</span>
+			{/* test */}
+			<span>{JSON.stringify(session)}</span>
 			<Button>
 				<ArrowLeft className="mr-4" />
 				<Link href={"/"}>Home</Link>
