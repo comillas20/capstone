5049ababca8f51837eb3e449d293fe077f52f8f9
comillas20 <jinojoycomillas20@gmail.com>
2024-01-06 14:19:32 +0800

    .

--- app/api/paymongo/route.ts
+++ app/api/paymongo/route.ts
@@ -1,22 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Readable } from "stream";
 
 // Helper function to get raw body
-async function getRawBody(readable: Readable): Promise<Buffer> {
-	const chunks = [];
-	for await (const chunk of readable) {
-		chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
-	}
-	return Buffer.concat(chunks);
-}
+// async function getRawBody(readable: Readable): Promise<Buffer> {
+// 	const chunks = [];
+// 	for await (const chunk of readable) {
+// 		chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+// 	}
+// 	return Buffer.concat(chunks);
+// }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
 	console.log("Yes, this was called");
 	// This is where you handle the incoming webhook event
-	const rawBody = await getRawBody(req);
+	// const rawBody = await getRawBody(req);
+	const rawBody = await req.json();
 	const event = req.body;
-
+	console.log("event", event);
 	console.log("Raw body for this request is:", rawBody);
 
 	// Parse the raw body into JSON
@@ -27,10 +28,11 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
 
 	// PayMongo adds a `Paymongo-Signature` HTTP header to the data that they send to your webhook
 	// This header can be used to verify that the request came from PayMongo
-	const signature = req.headers["paymongo-signature"];
+	// const signature = req.headers["paymongo-signature"];
 
 	// TODO: Verify the signature
 
 	// Always respond with a 200 or 2xx after receiving the webhook event
-	res.status(200).json({ received: true });
+	// res.status(200).json({ received: true });
+	return new Response(null, { status: 200 });
 }
