566c66846e0916f803202ea74055314d430cd423
comillas20 <jinojoycomillas20@gmail.com>
2024-01-07 15:24:33 +0800

    adding a reservation when paymongo sends their processed data(reply) here

--- app/api/paymongo/checkout_payment/route.ts
+++ app/api/paymongo/checkout_payment/route.ts
@@ -1,10 +1,76 @@
+import prisma from "@lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
+type RequestType = {
+	data: {
+		attributes: {
+			type: "checkout_session.payment.paid";
+			data: {
+				attributes: {
+					line_items: {
+						amount: number;
+						currency: "PHP";
+						name: string;
+						quantity: number;
+					}[];
+					payments: {
+						id: string;
+						type: "payment";
+						attributes: {
+							amount: number;
+							balance_transaction_id: string;
+							currency: "PHP";
+							external_reference_number: number | null;
+							fee: number;
+							net_amount: number;
+							payment_intent_id: string;
+							status: "paid";
+							tax_amount: number;
+							metadata: {
+								message: string;
+								eventDate: string;
+								eventDuration: string;
+								userID: string;
+								dishes: string;
+							};
+						};
+					}[];
+					reference_number: string;
+					status: "active";
+				};
+			};
+		};
+	};
+};
 export async function POST(req: NextRequest, res: NextResponse) {
-	console.log("Yes, this was called");
-	const rawBody = await req.json(); //this is where I get the event like when user successfully pays
-	console.log("Raw body for this request is:", rawBody);
+	const request: RequestType = await req.json(); //this is where I get the event like when user successfully pays
+	const newReservation = await prisma.reservations.create({
+		data: {
+			eventDate: new Date(
+				request.data.attributes.data.attributes.payments[0].attributes.metadata.eventDate
+			),
+			eventDuration: parseInt(
+				request.data.attributes.data.attributes.payments[0].attributes.metadata
+					.eventDuration
+			),
+			setName: request.data.attributes.data.attributes.line_items[0].name,
+			net_amount:
+				request.data.attributes.data.attributes.payments[0].attributes.net_amount /
+				100,
+			fee:
+				request.data.attributes.data.attributes.payments[0].attributes.fee / 100,
+			message:
+				request.data.attributes.data.attributes.payments[0].attributes.metadata
+					.message,
+			dishes: JSON.parse(
+				request.data.attributes.data.attributes.payments[0].attributes.metadata
+					.dishes
+			),
+			userID: parseInt(
+				request.data.attributes.data.attributes.payments[0].attributes.metadata
+					.userID
+			),
+		},
+	});
 	return new Response("ok", { status: 200 });
 }
-
-async function createReservation() {}
