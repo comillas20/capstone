9a50570b910679195db2692baf8bb6e447b4050f
comillas20 <jinojoycomillas20@gmail.com>
2024-01-19 19:25:45 +0800

    added SMS notification upon paying

--- app/api/paymongo/checkout_payment/route.ts
+++ app/api/paymongo/checkout_payment/route.ts
@@ -1,3 +1,4 @@
+import { sendSMS } from "@app/(website)/serverActionsGlobal";
 import prisma from "@lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -7,6 +8,11 @@ type RequestType = {
 			type: "checkout_session.payment.paid";
 			data: {
 				attributes: {
+					billing: {
+						email: string | null;
+						name: string | null;
+						phone: string | null;
+					};
 					line_items: {
 						amount: number;
 						currency: "PHP";
@@ -36,6 +42,7 @@ type RequestType = {
 								totalPaid: string;
 								totalCost: string;
 								venueID: string;
+								phoneNumber: string;
 							};
 						};
 					}[];
@@ -49,6 +56,37 @@ type RequestType = {
 export async function POST(req: NextRequest, res: NextResponse) {
 	try {
 		const request: RequestType = await req.json(); //this is where I get the event, like when user successfully pays
+		const eventDate = new Date(
+			request.data.attributes.data.attributes.payments[0].attributes.metadata.eventDate
+		);
+		const eventDuration = parseInt(
+			request.data.attributes.data.attributes.payments[0].attributes.metadata
+				.eventDuration
+		);
+		const eventType =
+			request.data.attributes.data.attributes.payments[0].attributes.metadata
+				.eventType;
+		const setName = request.data.attributes.data.attributes.line_items[0].name;
+		const dishes = JSON.parse(
+			request.data.attributes.data.attributes.payments[0].attributes.metadata
+				.dishes
+		) as string[];
+		const userID = parseInt(
+			request.data.attributes.data.attributes.payments[0].attributes.metadata
+				.userID
+		);
+		const totalPaid = parseFloat(
+			request.data.attributes.data.attributes.payments[0].attributes.metadata
+				.totalPaid
+		);
+		const totalCost = parseFloat(
+			request.data.attributes.data.attributes.payments[0].attributes.metadata
+				.totalCost
+		);
+		const venueID = parseFloat(
+			request.data.attributes.data.attributes.payments[0].attributes.metadata
+				.venueID
+		);
 		const reservation = await prisma.transactions.create({
 			data: {
 				fee:
@@ -62,43 +100,50 @@ export async function POST(req: NextRequest, res: NextResponse) {
 						.message,
 				reservation: {
 					create: {
-						eventDate: new Date(
-							request.data.attributes.data.attributes.payments[0].attributes.metadata.eventDate
-						),
-
-						eventDuration: parseInt(
-							request.data.attributes.data.attributes.payments[0].attributes.metadata
-								.eventDuration
-						),
-						eventType:
-							request.data.attributes.data.attributes.payments[0].attributes.metadata
-								.eventType,
-						setName: request.data.attributes.data.attributes.line_items[0].name,
-
-						dishes: JSON.parse(
-							request.data.attributes.data.attributes.payments[0].attributes.metadata
-								.dishes
-						),
-						userID: parseInt(
-							request.data.attributes.data.attributes.payments[0].attributes.metadata
-								.userID
-						),
-						totalPaid: parseFloat(
-							request.data.attributes.data.attributes.payments[0].attributes.metadata
-								.totalPaid
-						),
-						totalCost: parseFloat(
-							request.data.attributes.data.attributes.payments[0].attributes.metadata
-								.totalCost
-						),
-						venueID: parseFloat(
-							request.data.attributes.data.attributes.payments[0].attributes.metadata
-								.venueID
-						),
+						eventDate: eventDate,
+						eventDuration: eventDuration,
+						eventType: eventType,
+						setName: setName,
+						dishes: dishes,
+						userID: userID,
+						totalPaid: totalPaid,
+						totalCost: totalCost,
+						venueID: venueID,
 					},
 				},
 			},
 		});
+
+		const venue = await prisma.venues.findUnique({
+			where: {
+				id: venueID,
+			},
+			select: {
+				location: true,
+			},
+		});
+		// send sms
+		const message = `Your reservation in Jakelou has been successfully reserved.\n\n
+		Your reservation details:\n
+		Reference no.: ${request.data.attributes.data.attributes.reference_number}\n
+		Event Date: ${eventDate}\n
+		${venue ? "Where: " + venue.location : ""}\n
+		How long: ${eventDuration} hours\n
+		What kind of event: ${eventType}\n
+		Amount paid: ${totalPaid}\n
+		Total cost: ${totalCost}\n\n
+		${
+			totalCost > totalPaid
+				? "Please pay in full before 3 days prior to the event date, failure to do so may result your reservation being cancelled."
+				: ""
+		}`;
+
+		const phoneNumber = request.data.attributes.data.attributes.billing.phone;
+		if (phoneNumber)
+			await sendSMS({
+				message: message,
+				recipient: phoneNumber,
+			});
 		return new Response("ok", { status: 200 });
 	} catch (error) {
 		return new Response("not ok", { status: 200 });
