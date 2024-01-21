ce4481af435d46a07cd59ef7528584c1aa123c7f
comillas20 <jinojoycomillas20@gmail.com>
2024-01-21 17:19:49 +0800

    getDaysBySubraction of two dates function

--- lib/date-utils.ts
+++ lib/date-utils.ts
@@ -16,3 +16,24 @@ export function findNearestNonDisabledDate(
 		nearestDate.setDate(nearestDate.getDate() + 1);
 	}
 }
+
+/**
+ *
+ * @param first a Date
+ * @param second also a Date
+ *
+ * the function simply does first - second, and returns te difference
+ * @returns the difference of days of the two Date objects provided
+ */
+export function getDaysBySubtraction(first: Date, second: Date) {
+	// Set time to midnight (00:00:00) for both dates
+	first.setHours(0, 0, 0, 0);
+	second.setHours(0, 0, 0, 0);
+
+	// Calculate the difference in milliseconds
+	const timeDifference = first.getTime() - second.getTime();
+
+	// Convert the time difference to days
+	const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+	return daysDifference;
+}
